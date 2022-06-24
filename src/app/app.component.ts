67b000dc792084393b0a71e8c87ec58739531f13
zishan <macone394@gmail.com>
2022-06-24 12:29:35 +0530

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
 
-  getStr(name:any){
-    return alert("hello "+name);
-  }
+  
+
+render_value='';
+val1: string='';
+
+ getValue(val:string)
+ {
+  console.log(val);
+  this.render_value=val;
+  
+ }
+
+count_1=0;
+
+inc_dec_val(val1:string){
+
+  val1=='add' ?this.count_1++:this.count_1--;
+
+  console.log(this.count_1);
+  
+}
 
 
-  getData(val:string|undefined){
-    console.warn(val);
-    
-  }
+
 }
